4d9b3994dc9dc93d8b5d900729529efe22825397
Felix-fale <falefelix23@gmail.com>
2022-12-09 00:14:50 +0000

    reuse a component

--- src/Home.js
+++ src/Home.js
@@ -17,11 +17,22 @@ function Home() {
       author: "tony",
       date: "08/12/2022",
     },
+    {
+      id: 3,
+      title: "Lorem, troisieme.",
+      body: "Lorem ipsum dolor sit amet consectetur",
+      author: "tony",
+      date: "08/12/2022",
+    },
   ]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={'Liste des blogs'}></BlogList>
+      <BlogList blogs={blogs} title={"Liste des blogs"}></BlogList>
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "tony")}
+        title={"Liste des blogs de tony"}
+      ></BlogList>
     </div>
   );
 }
