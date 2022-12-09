633ed83702e883eab66fa1c205273225fe86eb81
Felix-fale <falefelix23@gmail.com>
2022-12-09 08:39:50 +0000

    add loading bar

--- src/Home.js
+++ src/Home.js
@@ -4,27 +4,25 @@ import BlogList from "./BlogList";
 function Home() {
   const [blogs, setBlog] = useState(null);
 
-  const [name, setName] = useState("fale");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlog(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setBlog(data);
+          setIsLoading(false);
+        });
+    }, 2000);
   }, []);
 
   return (
     <div className="home">
-      {blogs && (
-        <BlogList
-          blogs={blogs}
-          title={"Liste des blogs"}
-        ></BlogList>
-      )}
+      {isLoading && <div>En cours de traitement ...</div>}
+      {blogs && <BlogList blogs={blogs} title={"Liste des blogs"}></BlogList>}
     </div>
   );
 }
