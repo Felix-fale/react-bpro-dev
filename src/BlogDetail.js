e0972b0c1b69c4fa5d8423e03369ea122b548ed3
Felix-fale <falefelix23@gmail.com>
2022-12-10 13:42:05 +0000

    make a delete request to delete the articles

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,13 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 function BlogDetail(props) {
   const { id } = useParams();
+
   const {
     data: blog,
     isLoading,
     error,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const history = useHistory();
+
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+      console.log("supprimer avec succes");
+    });
+  };
 
   return (
     <div className="">
@@ -16,9 +28,12 @@ function BlogDetail(props) {
       {blog && (
         <div className="blog">
           <h2 className="blog-titre">{blog.title} </h2>
-          <small className="blog-publication-date">{`Publié le:${blog.date}`} </small>
+          <small className="blog-publication-date">
+            {`Publié le:${blog.date}`}{" "}
+          </small>
           <p className="blog-body">{blog.body} </p>
           <p className="blog-author">{`Publié par: ${blog.author}`} </p>
+          <button onClick={handleDelete}>Supprimer</button>
         </div>
       )}
     </div>
