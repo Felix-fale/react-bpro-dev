a898813260ffea498dd493228e4df865a7bd6c81
Felix-fale <falefelix23@gmail.com>
2022-12-10 12:14:08 +0000

    control form input fields

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,63 @@
+import { useState } from "react";
 
-function create(props) {
-    return (
-        <div className="create">
-            <h2>Ajouter  un nouveau article</h2>
+function Create(props) {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
+  return (
+    <div className="create-blog">
+      <form className="form">
+        <div className="form-group">
+          <label htmlFor="title">Titre de l'article</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
         </div>
-    );
+        <div className="form-group">
+          <label htmlFor="author">Selectionez un auteur</label>
+          <select
+            required
+            className="form-control"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value=""></option>
+            <option value="Fale">Duplex</option>
+            <option value="Tony">Tony</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="body">Contenu de l'article</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            className="form-control textarea"
+            id="body"
+            rows="10"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          {/* {!isLoading && (
+            <button type="submit" className="btn-create">
+              Creer Article
+            </button>
+          )}
+          {isLoading && (
+            <button type="submit" className="btn-create" disabled>
+              En cour de traitement ...
+            </button>
+          )} */}
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default create;
+export default Create;
